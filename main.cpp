70af4c034bc3ce0d1434faf829c148929fa72952
mxwlc <94302084+mxwlc@users.noreply.github.com>
2022-09-28 23:14:48 +0100

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "complex_number.h"
+#include <iostream>
+
+int main()
+{
+    bool calculate_again = true;
+    do
+    {
+        std::cout << "Do you want to add "
+    } while (calculate_again);
+    
+}
